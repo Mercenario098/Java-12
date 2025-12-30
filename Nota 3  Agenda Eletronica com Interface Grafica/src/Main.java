7ad837bdbc12a8348be1b6253c04ae8088094301
Mercenario098 <lucas.azevedo_@outlook.com.br>
2025-12-30 17:54:33 -0300

    add botão

--- Nota 3  Agenda Eletronica com Interface Grafica/src/Main.java
+++ Nota 3  Agenda Eletronica com Interface Grafica/src/Main.java
@@ -1,3 +1,59 @@
-void main() {
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public static void main() {
+
+    JFrame janela = new JFrame();
+    JButton botaoCadastar;
+    JButton botaoBuscar;
+    JButton botaoExcluir;
+
+    //Configuração da Janela
+    janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    janela.setTitle("Agenda Eletrônica");
+    janela.setSize(800, 600);
+    janela.setLayout(null);
+    janela.setLocationRelativeTo(null);
+    janela.setVisible(true);
+
+    //Botão Cadastrar
+    botaoCadastar =  new JButton("Cadastrar");
+    botaoCadastar.setBounds(350, 100, 100, 30);
+    botaoCadastar.addActionListener(new ActionListener() {
+        public void actionPerformed(ActionEvent e) {
+            String nome = IO.readln("Digite o nome: ");
+            String telefone = IO.readln("Digite o telefone: ");
+            String email = IO.readln("Digite o email: ");
+            String cpf = IO.readln("Digite o cpf: ");
+
+            Contato contatos = new Contato(nome, telefone, email, cpf);
+        }
+    });
+
+    //Botão Buscar
+    botaoBuscar = new JButton("Buscar");
+    botaoBuscar.setBounds(350, 150, 100, 30);
+    botaoBuscar.addActionListener(new ActionListener() {
+        public void actionPerformed(ActionEvent e) {
+
+        }
+    });
+
+    //Botão Excluir
+    botaoExcluir = new JButton("Excluir");
+    botaoExcluir.setBounds(350, 200, 100, 30);
+    botaoExcluir.addActionListener(new ActionListener() {
+        public void actionPerformed(ActionEvent e) {
+
+        }
+    });
+
+    //Configuração de botão na Janela
+    janela.add(botaoCadastar);
+    janela.add(botaoBuscar);
+    janela.add(botaoExcluir);
 
 }
